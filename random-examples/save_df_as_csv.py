ec3071c33761071e5e46d1a38bb46c17be9542c1
Costas Phot <georgephotsch@gmail.com>
2026-03-04 09:33:31 +0200

    Updated 'save_df_as_csv.py' with "Read a CSV" examples (written in a docs).

--- random-examples/save_df_as_csv.py
+++ random-examples/save_df_as_csv.py
@@ -2,6 +2,19 @@
 
 import pandas as pd
 
+"""
+  Read a CSV:
+1. Local file:
+  df = pd.read_csv("sample.csv")
+
+2. From the Internet:
+  df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/school_earnings.csv")
+
+3. From Google Drive:
+  from google.colab import drive
+  df = pd.read_csv("/content/drive/My Drive/Colab Notebooks/sample.csv")
+"""
+
 raw_data = {
     'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'], 
     'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'], 
